9759d5ef67e7df0711878c33e84ae63a2453e316
Radosław Warzocha <rwar@softserveinc.com>
2017-04-08 15:07:53 +0200

    Rewritten tests - we should start small

--- sqlalchemy_modelstatus/test/test_statusmodel.py
+++ sqlalchemy_modelstatus/test/test_statusmodel.py
@@ -1,24 +1,44 @@
 # -*- encoding: utf-8 -*-
 
 import pytest
+
+from sqlalchemy import Column, Integer
 from sqlalchemy.ext.declarative import declarative_base
 
-from ..statusmodel import *
+from ..status_model_mixin import *
 from ..exceptions import *
 
 
 pytestmark = pytest.mark.usefixtures("db_engine")
 
 
+@pytest.fixture(scope='module')
+def status_dict_basic():
+    return {
+        'status': {
+            'access_pending': 'tc_pending',
+            'tc_pending': ['access_denied', 'offline'],
+            'access_denied': ['offline', 'locked', 'tc_pending'],
+            'offline': ['access_denied', 'online', 'locked'],
+            'online': 'offline',
+            'locked': 'offline'
+        },
+        'default': 'access_pending'
+    }
+
+
 @pytest.fixture(scope="module")
 def status_dict_status_based():
     return {
-        'access_pending': ('grant_access', 'tc_pending'),
-        'tc_pending': [('deny_access', 'access_denied'), ('accept_tc', 'offline')],
-        'access_denied': ('grant_access', ['offline', 'locked', 'tc_pending'], PREVIOUS_STATUS),
-        'offline': [('deny_access', 'access_denied'), ('log_in', 'online'), ('lock', 'locked')],
-        'online': ('log_out', 'offline'),
-        'locked': ('reset_password', 'offline'),
+        'status': {
+            'access_pending': ('grant_access', 'tc_pending'),
+            'tc_pending': [('deny_access', 'access_denied'), ('accept_tc', 'offline')],
+            'access_denied': ('grant_access', ['offline', 'locked', 'tc_pending'], PREVIOUS_STATUS),
+            'offline': [('deny_access', 'access_denied'), ('log_in', 'online'), ('lock', 'locked')],
+            'online': ('log_out', 'offline'),
+            'locked': ('reset_password', 'offline')
+        },
+        'default': 'access_pending',
         'groups': {
             'active': ['online', 'offline', 'locked']
         }
@@ -50,146 +70,86 @@ Base = declarative_base()
 # those fixtures are indirectly testing if the
 # model table can be created by SQLAlchemy
 @pytest.fixture(scope="module")
-def ModelWithStatus(db_engine, status_dict_status_based):
-    class SimpleModelWithStatus(StatusModel, Base):
-        __table__ = 'simple_model_status'
-        __status__ = status_dict_status_based
+def ModelWithBasicStatus(db_engine, status_dict_basic):
+    class ModelWithBasicStatus(StatusModelMixin, Base):
+        __tablename__ = 'basic_model_status'
+        __status_definition__ = status_dict_basic
 
-    db_engine.create_all()
+        id = Column(Integer, primary_key=True)
+
+    Base.metadata.create_all(db_engine)
+
+    yield ModelWithBasicStatus
+
+    Base.metadata.drop_all(db_engine)
+
+
+@pytest.fixture(scope="module")
+def ModelWithStatus(db_engine, status_dict_status_based):
+    class SimpleModelWithStatus(StatusModelMixin, Base):
+        __table__ = 'simple_model_status'
+        __status_definition__ = status_dict_status_based
+
+    Base.metadata.create_all(db_engine)
 
     yield SimpleModelWithStatus
 
-    db_engine.drop_all()
+    Base.metadata.drop_all(db_engine)
 
 
 @pytest.fixture(scope="module")
 def ModelWithActionStatus(db_engine, status_dict_action_based):
     class SimpleModelWithStatus:
         __table__ = 'action_model_status'
-        __status__ = status_dict_action_based
+        __status_definition__ = status_dict_action_based
 
-    db_engine.create_all()
+    Base.metadata.create_all(db_engine)
 
     yield SimpleModelWithStatus
 
-    db_engine.drop_all()
+    Base.metadata.drop_all(db_engine)
+
+
+@pytest.fixture
+def model_with_basic_status(ModelWithBasicStatus):
+    mwbs = ModelWithBasicStatus()
+    mwbs.status = 'access_pending'
+    return mwbs
 
 
 @pytest.fixture
 def model_with_status(ModelWithStatus):
     mws = ModelWithStatus()
-    mws.status = 'access_pending'
     return mws
 
 
 @pytest.fixture
 def model_with_action_status(ModelWithActionStatus):
     mwas = ModelWithActionStatus()
-    mwas.status = 'access_pending'
     return mwas
 
 
-def test_has_status_attr(model_with_status, model_with_action_status):
-    mws = model_with_status
-    mwas = model_with_action_status
-
-    assert hasattr(mws, 'status')
-    assert hasattr(mws, 'previous_status')
-    assert hasattr(mwas, 'status')
-    assert not hasattr(mwas, 'previous_status')
+def test_has_status_attr(model_with_basic_status):
+    mwbs = model_with_basic_status
+    assert hasattr(mwbs, 'status')
+    assert hasattr(mwbs, 'statuses')
+    assert hasattr(mwbs.statuses, 'ACCESS_PENDING')
 
 
-def test_has_checks(model_with_status, model_with_action_status):
-    mws = model_with_status
-    mwas = model_with_action_status
-
-    # status should be access pending, it's set in the fixtures
-    assert mws.is_access_pending()
-    assert not mws.is_access_denied()
-    assert not mws.is_tc_pending()
-    assert not mws.is_ofline()
-    assert not mws.is_online()
-    assert not mws.is_locked()
-
-    assert mwas.is_access_pending()
-    assert not mwas.is_access_denied()
-    assert not mwas.is_tc_pending()
-    assert not mwas.is_ofline()
-    assert not mwas.is_online()
-    assert not mwas.is_locked()
-    
-
-def test_has_group_checks(model_with_status, model_with_action_status):
-    mws = model_with_status
-    mwas = model_with_action_status
-
-    assert not mws.is_active()
-    assert not mwas.is_active()
+def test_get_status(model_with_basic_status):
+    mwbs = model_with_basic_status
+    assert mwbs.status == mwbs.statuses.ACCESS_PENDING
 
 
-def test_simple_transitions(model_with_status, model_with_action_status):
-    mws = model_with_status
-    mwas = model_with_action_status
+def test_set_status(model_with_basic_status):
+    mwbs = model_with_basic_status
+    mwbs.status = mwbs.statuses.TC_PENDING
+    assert mwbs.status == mwbs.statuses.TC_PENDING
 
-    assert mws.is_access_pending()
-    mws.grant_access()
-    assert mws.is_tc_pending()
-    mws.accept_tc()
-    assert mws.is_offline()
-    mws.log_in()
-    assert mws.is_online()
-    mws.log_out()
-    assert mws.is_offline()
-    mws.lock()
-    assert mws.is_locked()
-    mws.reset_password()
-    assert mws.is_offline()
+
+def test_bad_transition_exception(model_with_basic_status):
+    mwbs = model_with_basic_status
 
     with pytest.raises(StatusTransitionException):
-        mws.accept_tc()
+        mwbs.status = mwbs.statuses.LOCKED
 
-    assert mwas.is_access_pending()
-    mwas.grant_access()
-    assert mwas.is_tc_pending()
-    mwas.accept_tc()
-    assert mwas.is_offline()
-    mwas.log_in()
-    assert mwas.is_online()
-    mwas.log_out()
-    assert mwas.is_offline()
-    mwas.lock()
-    assert mwas.is_locked()
-    mwas.reset_password()
-    assert mwas.is_offline()
-
-    with pytest.raises(StatusTransitionException):
-        mwas.accept_tc()
-
-
-def test_previous_transition(model_with_status):
-    mws = model_with_status
-
-    mws.grant_access()
-    mws.deny_access()
-    mws.grant_access()
-
-    assert mws.is_tc_pending()
-
-    mws.accept_tc()
-    mws.deny_access()
-    mws.grant_access()
-
-    assert mws.is_offline()
-
-
-def test_any_transition(model_with_action_status):
-    mwas = model_with_action_status
-
-    mwas.grant_access()
-    assert mwas.is_tc_pending()
-    mwas.deny_access()
-    assert mwas.is_access_denied()
-    mwas.grant_access()
-    assert mwas.is_offline()
-    mwas.deny_access()
-    assert mwas.is_access_denied()
